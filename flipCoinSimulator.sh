b11355f0a5dce3207075e2f50d163b2f0fcab0c7
TahirMansuri <tprocks786@gmail.com>
2021-11-28 13:50:16 +0530

    MODIFIED Fliping Coin MultipleTimes

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,11 +6,14 @@ echo "Welcome to Flip Coin Simulator Program";
 headCount=0;
 tailCount=1;
 
-flip=$((RANDOM%2));
-
-if (( $flip == $headCount ))
-then
-	echo "Head Wins!!!";
-else
-	echo "Tail Wins!!!";
-fi
+#Fliping Coin Upto 10 Times
+for (( i=1; i<=10; i++ ))
+do
+	flip=$((RANDOM%2));
+	if (( $flip == $headCount ))
+	then
+		echo "Head Wins!!!";
+	else
+		echo "Tail Wins!!!";
+	fi
+done
